1f3dd2f3bea79876f45499cf73925f6d6f09d9b0
Mandy Noto <mandynoto@icloud.com>
2023-05-12 18:51:53 -0700

    feat(1iteratordemo.rb): create integer, array, and hash

---
+++ code/1iteratordemo.rb
@@ -0,0 +1,4 @@
+i=4; # initializes an empty integer for counting
+names=["john","peter","job"] # initializes an array of names
+abbreviations={a:"alpha", b:"beta", c:"charlie"} # is a hash of abbreviations
+
