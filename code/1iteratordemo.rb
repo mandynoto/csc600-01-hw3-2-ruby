9892bd687d7c49b1aef4e031c4c4b47b02555760
Mandy Noto <mandynoto@icloud.com>
2023-05-12 18:53:01 -0700

    feat(1iteratordemo.rb): demonstrate while

--- code/1iteratordemo.rb
+++ code/1iteratordemo.rb
@@ -2,3 +2,4 @@ i=4; # initializes an empty integer for counting
 names=["john","peter","job"] # initializes an array of names
 abbreviations={a:"alpha", b:"beta", c:"charlie"} # is a hash of abbreviations
 
+print"while:\t\t"    ; print "#{i-=1}" while i>1 # counts down from 3 to 1
