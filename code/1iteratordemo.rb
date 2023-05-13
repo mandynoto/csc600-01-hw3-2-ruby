61b5148a3165f834682d87fa8992939f10cf037d
Mandy Noto <mandynoto@icloud.com>
2023-05-12 18:53:57 -0700

    feat(1iteratordemo): demonstrate upto

--- code/1iteratordemo.rb
+++ code/1iteratordemo.rb
@@ -5,3 +5,4 @@ abbreviations={a:"alpha", b:"beta", c:"charlie"} # is a hash of abbreviations
 print"while:\t\t"    ; print "#{i-=1}" while i>1 # counts down from 3 to 1
 print"\nuntil:\t\t"  ; until i>3 ;print i ; i+=1 end# counts up to 3
 print"\nfor:\t\t"    ; for name in names; print"#{name} " end
+print"\nupto:\t\t"   ; 4.upto(7){|n|print n}
