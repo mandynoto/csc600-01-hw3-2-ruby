c874a82b81e80cb61c832f3cda622040b0feea44
Mandy Noto <mandynoto@icloud.com>
2023-05-12 18:53:06 -0700

    feat(1iteratordemo.rb): demonstrate until

--- code/1iteratordemo.rb
+++ code/1iteratordemo.rb
@@ -3,3 +3,4 @@ names=["john","peter","job"] # initializes an array of names
 abbreviations={a:"alpha", b:"beta", c:"charlie"} # is a hash of abbreviations
 
 print"while:\t\t"    ; print "#{i-=1}" while i>1 # counts down from 3 to 1
+print"\nuntil:\t\t"  ; until i>3 ;print i ; i+=1 end# counts up to 3
