5d3dc3d5846d9c8f389b445a509ca4ee383672a3
Mandy Noto <mandynoto@icloud.com>
2023-05-14 19:52:18 -0700

    feat(2): implement limited?

--- code/2recognizer.rb
+++ code/2recognizer.rb
@@ -1,2 +1,8 @@
 class Array
+	#  Return true if amin <= a[i] <=amax for all values of i.
+	def limited?(amin,amax)
+		# Try to disprove the range
+		for e in self do return false if amin>e||e>amax end
+		return true # implies disapproval failed
+	end
 end
