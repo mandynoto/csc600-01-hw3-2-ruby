77f801c59916aeb176b37de4fa2df0cdbc70a2bd
Mandy Noto <mandynoto@icloud.com>
2023-05-14 19:52:26 -0700

    feat(2): implement sorted?

--- code/2recognizer.rb
+++ code/2recognizer.rb
@@ -5,4 +5,12 @@ class Array
 		for e in self do return false if amin>e||e>amax end
 		return true # implies disapproval failed
 	end
+	# Return 0,-1,1 depending on how array is sorted.
+	def sorted?
+		inc,dec=true,true # assumes array is sorted in either direction
+		0.upto self.length-2 do |i| # tries to disprove assumption
+			inc=false if self[i]>self[i+1]; dec=false if self[i]<self[i+1]
+		end
+		if inc then 1 elsif dec then -1 else 0 end # concludes assumption
+	end
 end
