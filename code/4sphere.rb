a8e40971eae4cacfa15b9d1a78bfbc6dcf0c2ee8
Mandy Noto <mandynoto@icloud.com>
2023-05-19 13:04:04 -0700

    style(4c): pack code more

--- code/4sphere.rb
+++ code/4sphere.rb
@@ -1,28 +1,15 @@
 class Sphere
-	def initialize(r)
-		@radius=r
-	end
-	def area
-		4*(@radius**2)*Math::PI
-	end
-	def volume
-		(4*(@radius**3)*Math::PI)/3
-	end
+	def initialize(r); @radius=r end
+	def area; 4*(@radius**2)*Math::PI end # returns this sphere's area
+	def volume; (4*(@radius**3)*Math::PI)/3 end # returns this sphere's volume
 end
 class Ball < Sphere
-	def initialize(r,c)
-		super(r)
-		@color=c
-	end
+	def initialize(r,c); super(r); @color=c end
 end
 class MyBall < Ball
-	def initialize(r,c,o)
-		super(r,c)
-		@owner=o
-	end
-	def show
-		print "@radius=#{@radius}\n@color=#{@color}\n@owner=#{@owner}"
-	end
+	def initialize(r,c,o); super(r,c); @owner=o end
+	# Show current and inherited instance variables
+	def show; print "@radius=#{@radius}\n@color=#{@color}\n@owner=#{@owner}" end
 end
 
 mb=MyBall.new(5,"red","jon")
